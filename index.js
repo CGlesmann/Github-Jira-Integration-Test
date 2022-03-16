4ee4b008b6d05829110ac8218b4755617fd7efd6
Chris Glesmann <chrisglesmann@gmail.com>
2022-03-16 11:15:46 -0700

    aowiufhaiwuhf

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ function helloWorld()
     console.log("world");
     console.log("789");
     console.log("145623");
+<<<<<<< Updated upstream
     console.log("adiawgudhaw")
+=======
+    console.log("adiawgudhaw");
+    console.log("aoihgeuoiwg");
+    console.log("sefoiusehgfisuegh");
+    console.log("iuawehfiawhfuih");
+    console.log("adouiwahdiuawhdiuawd");
+    console.log(awldjkawhjdaiwd);
+>>>>>>> Stashed changes
 }
 
 helloWorld();
